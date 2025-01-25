8eb9cbeec818f9cace56b46c63bd69c8d19ba9af
Will Spencer <willspencer171@gmail.com>
2025-01-25 15:30:58 +0000

    Created BFS / DFS for Vacuum world problem

---
+++ Artificial Intelligence and Machine Learning/Programming/Week 2/vacuum_world.py
@@ -0,0 +1,68 @@
+from uninformed_search1 import Node, StackFrontier, QueueFrontier
+import csv
+
+class VacuumWorldSolver:
+    def __init__(self, filename):
+        with open(filename) as f:
+            reader = csv.reader(f)
+            contents = [row for row in reader]
+            headers = contents.pop(0)
+        print(contents)
+
+        self.frontier = QueueFrontier()
+        self.solution = None
+
+        self.goals = [7, 8]
+    
+    def neighbours(self, state):
+        int_state = int(state)
+        suck_states = [3,6,3,8,7,6,7,8]
+
+        candidate_actions = [
+            ('left', (state if int_state % 2 == 1 else int_state - 1)),
+            ('right', (state if int_state % 2 == 0 else int_state + 1)),
+            ('suck', suck_states[int_state-1]) 
+        ]
+        neighbours = []
+        for action, res in candidate_actions:
+            if res != state:
+                neighbours.append((action, res))
+        
+        return neighbours
+
+    def solve(self):
+        start = Node('1', None, None)
+        self.frontier.add(start)
+
+        self.explored = set()
+
+        while True:
+            if self.frontier.isempty:
+                raise Exception("No solution found!")
+            
+            node = self.frontier.pop()
+
+            if node.state in self.goals:
+                actions = []
+                cells = []
+
+                while node.parent:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+            
+            self.explored.add(node.state)
+
+            for action, state in self.neighbours(node.state):
+                if not self.frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state, node, action)
+                    self.frontier.add(child)
+
+if __name__ == '__main__':
+    world = VacuumWorldSolver('vacuum_world.csv')
+    world.solve()
